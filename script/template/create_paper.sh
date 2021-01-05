a67b2809243f8e2dc4f33a0d9c86693928fd8639
TheNetAdmin <zxwang42@gmail.com>
2021-01-05 15:45:04 -0800

    Add script to create paper project

---
+++ script/template/create_paper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 PAPER_NAME GITHUB_REPO"
+    echo "E.g. $0 awesome TheNetAdmin/paper-awesome"
+fi
+
+paper_name=$1
+github_repo=git@github.com:$2
+
+git clone git@github.com:TheNetAdmin/nap $1
+
+cd $1
+git remote set-url origin $github_repo
+git push origin -u master
+git checkout -b paper
+git push origin -u paper
