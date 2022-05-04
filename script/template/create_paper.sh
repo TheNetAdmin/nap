5fef387b351e4821ba4d6edf7302977338c4d863
Zixuan Wang <zxwang42@gmail.com>
2022-05-04 13:56:29 -0700

    Update create paper script

--- script/template/create_paper.sh
+++ script/template/create_paper.sh
@@ -3,12 +3,13 @@
 set -e
 
 if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
-    echo "Usage: $0 PAPER_NAME GITHUB_REPO"
+    echo "Usage: $0 PAPER_NAME [GITHUB_REPO]"
     echo "E.g. $0 awesome TheNetAdmin/paper-awesome"
+    echo "     will generate dir paper-awesome/ and link to the GitHub"
     exit 2
 fi
 
-paper_name=$1
+paper_name=paper-$1
 
 if [ $# -eq 2 ]; then
     github_repo=git@github.com:$2
@@ -19,10 +20,19 @@ fi
 echo "paper_name:  $paper_name"
 echo "github_repo: $github_repo"
 
+while true; do
+    read -p "Proceed? [y/n]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no";;
+    esac
+done
+
 git clone git@github.com:TheNetAdmin/nap $1
 
-cd $1
-git remote set-url origin $github_repo
+cd "${paper_name}"
+git remote set-url origin ${github_repo}
 git push origin -u master
 git checkout -b paper
 git push origin -u paper
